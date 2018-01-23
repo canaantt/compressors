7231aa349a2c8fe9bf92b78583aae0d01133598e
canaantt <canaantt@gmail.com>
2018-01-22 16:30:54 -0800

    .

--- converter.js
+++ converter.js
@@ -11,19 +11,22 @@ var ACCOUNT_PERMISSIONS = [];
 var ACCOUNT_PROJECTS = [];
 mongoose.connect(
     'mongodb://oncoscape-dev-db1.sttrcancer.io:27017,oncoscape-dev-db2.sttrcancer.io:27017,oncoscape-dev-db3.sttrcancer.io:27017/v2?authSource=admin', {
-        db: {
-            native_parser: true
-        },
-        server: {
-            poolSize: 5,
-            reconnectTries: Number.MAX_VALUE
-        },
-        replset: {
-            rs_name: 'rs0'
-        },
-        user: process.env.MONGO_USERNAME,
-        pass: process.env.MONGO_PASSWORD
-    });
+    db: {
+        native_parser: true
+    },
+    server: {
+        poolSize: 5,
+        reconnectTries: Number.MAX_VALUE
+    },
+    replset: {
+        rs_name: 'rs0'
+    },
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD
+});
+
+
+
 
 
 connection.once('open', function(){
@@ -34,8 +37,30 @@ connection.once('open', function(){
             return collName.split('_')[0].length === 24
         });
         userProjectIDs = _.uniq(userCollections.map(collName => collName.split('_')[0]));
-        
-
+        // console.log(userCollections.filter(collName => collName.indexOf(userProjectIDs[0]) > -1));
+        asyncLoop(userCollections, function(collectionName, next){ 
+            var collectionName = process.env.GOOD_CLINICAL;
+            console.log('******', collectionName);
+            var mongoData;
+            db.collection(collectionName).find().toArray(function(err, data){
+                mongoData = data;
+            });
+            var cnvMongoData;
+            db.collection(collectionName).find().toArray(function(err, data){
+                if(collectionName.indexOf("_CNV") > -1 || collectionName.indexOf("_EXPR") > -1) {
+                    Compression.compress_molecular(data);
+                }
+            });
+          }, function (err)
+          {
+              if (err)
+              {
+                  console.error('Error: ' + err.message);
+                  return;
+              }
+              console.log('Finished!');
+              connection.close();
+          });
     });
 });
 
@@ -43,8 +68,15 @@ var Compression = {
     compress_clinical: function() {
         return 'generic dough';
     },
-    compress_molecular: function() {
-        return 'generic sauce';
+    compress_molecular: function(molecularData) {
+        obj = {};
+        ids = molecularData[0].s;
+        genes = molecularData.map(c=>c.m);
+        values = molecularData.map(c=>c.d);
+        obj.ids = ids;
+        obj.genes = genes;
+        obj.values = values;
+        return obj;
     },
     compress_sample: function() {
 
