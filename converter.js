29c59ad036fde1ee1b292934969da78d92f7ff72
Jenny Zhang <canaantt@gmail.com>
2018-01-26 09:52:28 -0800

    .

--- converter.js
+++ converter.js
@@ -216,3 +216,6 @@ var Compression = {
 
 
 AWS.config.s3.region
+s3.config.region = 'us-west-2'
+var params = {Bucket:'canaantt-test'};
+s3.createBucket(params, function(err, data){ if(err) console.log(err, err.stack); else console.log(data);});
